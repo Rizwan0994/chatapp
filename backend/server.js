f4baf547b16b5be5de96b9fce1093bf3db334a4e
Rizwan0994 <f190994@nu.edu.pk>
2024-03-24 11:47:01 +0500

    path setup

--- backend/server.js
+++ backend/server.js
@@ -123,18 +123,21 @@ app.use("/api/chat", require("./routes/chatRoutes"));
 app.use("/api/message", require("./routes/messageRoutes"));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
+app.get("/", (req, res) => {
+  res.send("API is running..");
+});
 // Error handling middleware
  const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 // // const { Socket } = require("socket.io");
